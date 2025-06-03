6f4663757339e3833020c3a0cfdde3fcc788a6c2
Sufi111729 <sufi111729@gmail.com>
2025-06-03 13:50:16 +0530

    Add Spring Boot project structure

---
+++ src/test/java/com/sufi/app/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sufi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
